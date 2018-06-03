fd4de941286802af8ce64b284c11b2991deaa321
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-03 11:51:31 -0700

    cli issue start show branch name

--- lib/commands/issue.rb
+++ lib/commands/issue.rb
@@ -1,3 +1,4 @@
+require 'facets/string/snakecase'
 require 'thor'
 require_relative '../github_flow_cli/api'
 require_relative '../github_flow_cli/config'
@@ -24,6 +25,17 @@ module GithubFlowCli
       list_issues(assignee: 'none')
     end
 
+    desc "start", "create a new branch named after the issue"
+    def start(number)
+      issue = API.issue(Local.repo, number)
+      # TODO: create branch based on RepoRules
+      # TODO: abstarct tag from issue title
+      branch_name = "TAG_#{number}_#{branch_name(issue.title)}"
+      puts branch_name
+    rescue Octokit::NotFound
+      puts "issue not found!"
+    end
+
     private
 
     def list_issues(config)
@@ -39,6 +51,10 @@ module GithubFlowCli
 
     def assignee_field(show_assignee, issue)
       show_assignee ? " (#{issue.assignee.login})" : ''
+    end
+
+    def branch_name(title)
+      title.gsub(/[^_\w\d ]/, '').snakecase
     end
   end
 end
