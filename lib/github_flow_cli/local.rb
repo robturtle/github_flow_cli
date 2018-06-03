ea53d1255b685ad784ee2203973da90a183a68a9
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-03 11:11:12 -0700

    fix repo matching

--- lib/github_flow_cli/local.rb
+++ lib/github_flow_cli/local.rb
@@ -5,7 +5,7 @@ module GithubFlowCli
     class << self
       def repo
         url = Git.open(File.expand_path('.')).remote.url
-        match = url&.match(%r{[:/](?<owner>.*?)/(?<name>.*?)\.git$})
+        match = url&.match(%r{.*[:/](?<owner>.*?)/(?<name>.*?)\.git$})
         return nil unless match[:owner] && match[:name]
         Octokit::Repository.from_url("/#{match[:owner]}/#{match[:name]}")
       rescue ArgumentError => ex
