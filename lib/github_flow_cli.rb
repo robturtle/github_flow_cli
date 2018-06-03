c99c8ccb7c5d2a2901224bb7e52301ca1345ca6d
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-03 09:44:30 -0700

    reorder requires

--- lib/github_flow_cli.rb
+++ lib/github_flow_cli.rb
@@ -1,5 +1,5 @@
-require "github_flow_cli/version"
-require "github_flow_cli/cli"
 require "github_flow_cli/api"
+require "github_flow_cli/cli"
 require "github_flow_cli/config"
 require "github_flow_cli/local"
+require "github_flow_cli/version"
