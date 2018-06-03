e18b42a9dec5c9f0b8df0b2b4d8894979c5d3c37
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-03 01:09:28 -0700

    format prompt text

--- lib/github_flow_cli/cli.rb
+++ lib/github_flow_cli/cli.rb
@@ -6,12 +6,12 @@ module GithubFlowCli
   class CLI < Thor
     desc "login", "login to Github with username and password"
     def login
-      Config.username = ask("Github username: ")
-      password = ask("password: ", echo: false)
+      Config.username = ask("Github username:")
+      password = ask("password:", echo: false)
       Config.oauth_token = authorize(Config.username, password)
       Config.save!
     rescue Config::BadConfig
-      puts "bad username or password, please try again."
+      puts "\nbad username or password, please try again."
       retry
     end
 
@@ -20,7 +20,7 @@ module GithubFlowCli
     def authorize(username, password)
       API.authorize(username, password)
     rescue Octokit::OneTimePasswordRequired
-      two_factor_token = ask("\nTwo Factor Authentication code: ")
+      two_factor_token = ask("\nTwo Factor Authentication code:")
       API.authorize(Config.username, password, two_factor_token: two_factor_token)
     end
   end
