30ec0798ae4b637b6427dbab930e29eb4c0b80bd
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-02 23:49:15 -0700

    cli login handles bad config

--- lib/github_flow_cli/cli.rb
+++ lib/github_flow_cli/cli.rb
@@ -1,14 +1,18 @@
 require 'thor'
 require_relative 'api'
+require_relative 'config'
 
 module GithubFlowCli
   class CLI < Thor
     desc "login", "login to Github with username and password"
     def login
-      username = ask("Github username: ")
+      Config.username = ask("Github username: ")
       password = ask("password: ", echo: false)
-      oauth_token = API.login(username, password)
-      # TODO: save jwt token
+      Config.oauth_token = API.login(Config.username, password)
+      Config.save!
+    rescue Config::BadConfig
+      puts "bad username or password, please try again."
+      retry
     end
   end
 end
