9b22679f083e7bf2a6344ca1630094bf5de59fc3
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-03 12:48:05 -0700

    cache Local.repo

--- lib/github_flow_cli/local.rb
+++ lib/github_flow_cli/local.rb
@@ -4,10 +4,11 @@ module GithubFlowCli
   class Local
     class << self
       def repo
+        return @repo if @repo
         url = git.remote.url
         match = url&.match(%r{.*[:/](?<owner>.*?)/(?<name>.*?)\.git$})
         return nil unless match[:owner] && match[:name]
-        Octokit::Repository.from_url("/#{match[:owner]}/#{match[:name]}")
+        @repo = Octokit::Repository.from_url("/#{match[:owner]}/#{match[:name]}")
       rescue ArgumentError => ex
         if ex.message =~ /path does not exist/
           nil
