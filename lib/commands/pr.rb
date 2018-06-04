79c5abf87595b8861e1cc944af20ed0b62148245
Yang Liu <jeremyrobturtle@gmail.com>
2018-06-03 17:15:12 -0700

    cli pr all

--- lib/commands/pr.rb
+++ lib/commands/pr.rb
@@ -6,7 +6,10 @@ module GithubFlowCli
   class PrCommands < Thor
     desc "all", "list all open PR"
     def all
-      puts "TODO"
+      pull_requests = API.pull_requests(Local.repo)
+      unless pull_requests.empty?
+        puts pull_requests.map { |p| "##{p.number} (#{p.assignee&.login || "NONE"}): #{p.title}" }
+      end
     end
   end
 end
